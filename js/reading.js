b0cdcf3f58f06b35b61590b318604621607d6c3c
Kevin Cabon <cabonk.pro@pm.me>
2023-01-30 17:43:53 +0100

    add new badges for reading page

--- js/reading.js
+++ js/reading.js
@@ -6,14 +6,27 @@ let getData = async () => {
         `
     document.getElementById("response").hidden = false
     let data = await _GetRequest("text")
+    let oneMonthAgo = new Date(
+        new Date().getFullYear(),
+        new Date().getMonth(), 
+        new Date().getDate() -7
+    )
     data.forEach(text => {
-        let badgeEsp = ""
-        let badgeFr = ""
+        let badgeEsp = "", newBadge = ""
+        let badgeFr = "", niveauBadge = ""
         badgeEsp = `<span class="bg-red-700 text-gray-200 text-xs px-2 py-1 rounded-full cursor-pointer" onclick="displayOriginalText('${text.id}')">Español</span>`
         badgeFr = `<span class="bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full cursor-pointer" onclick="displayTranslateText('${text.id}')">Français</span>`
+        if (!text.add_date){
+            newBadge = `<span class="bg-amber-600 text-gray-200 text-xs px-2 py-1 rounded-full italic">Coming Soon!</span>`
+        }else if (new Date(text.add_date) > new Date(oneMonthAgo)){
+            newBadge = `<span class="bg-green-600 text-gray-200 text-xs px-2 py-1 rounded-full">Nouveau !</span>`
+        }
+        if (text.niveau){
+            niveauBadge = `<span class="bg-cyan-600 text-gray-200 text-xs px-2 py-1 rounded-full font-bold">${text.niveau}</span>`
+        }
         document.getElementsByClassName("lecture__main")[0].innerHTML += `
             <article class="my-5 w-11/12 sm:w-10/12 md:w-9/12 lg:w-7/12 xl:w-6/12 p-3 mx-auto border border-current rounded-lg shadow-xl dark:shadow-slate-700 dark:shadow-lg bg-slate-300 dark:bg-gray-800 text-slate-600 dark:text-slate-300">
-                ${badgeEsp} ${badgeFr}
+                ${badgeEsp} ${badgeFr} <span class="float-right">${newBadge} ${niveauBadge}</span>
                 <div class="mt-3 text-justify" id="text_es_${text.id}">
                     <h4 class="font-bold">${text.title_es || ""}</h4>
                     ${text.text_es}
